5a5c02d2529ea8a9806fbdc1f69cf3fb744bf123
Lbongard <62773555+Lbongard@users.noreply.github.com>
2022-11-08 17:04:51 -0800

    Fixed time to PST and used more accurate load variables

--- Data Check DataCleaning.py
+++ Data Check DataCleaning.py
@@ -6,9 +6,10 @@ Data import, cleaning, and feature engineering for LMP Data
 import pandas as pd
 import numpy as np
 import glob
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from functools import reduce
 import requests
+
 from io import StringIO 
 
 ##### Import all LMP files and combine into a single DataFrame #####
@@ -34,21 +35,20 @@ def create_mult_csv_df(filepath):
     return df
     
 
-def utc_to_local(utc_dt):
-    """Converts UTC timezone to PST Time"""
-    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz='America/Los_Angeles')
 
 ## First Import RTLMP ##    
 
 rt_df = create_mult_csv_df("./Raw_Data/SP15_interval_LMP/")
 
-# Convert starting datetime to datetime type and add new PST version
-rt_df['INTERVALSTARTTIME_GMT'] = pd.to_datetime(rt_df['INTERVALSTARTTIME_GMT'] )
-rt_df['INTERVALSTARTTIME_PST'] = rt_df['INTERVALSTARTTIME_GMT'].apply(utc_to_local)
+rt_df_08_07 = rt_df[rt_df['OPR_DT'] == '2022-08-07']
+np.unique(rt_df_08_07['OPR_HR'])
 
+
+# Convert starting datetime to datetime type
+rt_df['INTERVALSTARTTIME_GMT'] = pd.to_datetime(rt_df['INTERVALSTARTTIME_GMT'] )
 
 # The data download from CAISO contains multiple types of prices. Convert from long to wide format
-rt_df = rt_df.pivot(index='INTERVALSTARTTIME_PST', columns='LMP_TYPE', values='VALUE')
+rt_df = rt_df.pivot(index='INTERVALSTARTTIME_GMT', columns='LMP_TYPE', values='VALUE')
 
 # Add prefix to cols for merging later
 rt_df.columns = ["RT_" + col for col in rt_df.columns]
@@ -56,18 +56,17 @@ rt_df.columns = ["RT_" + col for col in rt_df.columns]
 # Checking that LMP is roughly equivalent to the sum of all other lmp components at a given time
 sum((rt_df['RT_LMP'] - rt_df[['RT_MCC', 'RT_MCE', 'RT_MCL', 'RT_MGHG']].sum(axis=1)) < 0.001) / rt_df.shape[0]
 
-# Create hourly summary to merge with DA dataset
+# Create hourly summary to merge wwith DA dataset
 hourly_rt_df = rt_df.groupby(rt_df.index.floor("H")).mean()
 
 ## Now import DALMP ##
 
 da_df = create_mult_csv_df("./Raw_Data/SP15_DA_LMP/")
 
-# Convert starting datetime to datetime type and create PST datetime
+# Convert starting datetime to datetime type
 da_df['INTERVALSTARTTIME_GMT'] = pd.to_datetime(da_df['INTERVALSTARTTIME_GMT'])
-da_df['INTERVALSTARTTIME_PST'] = da_df['INTERVALSTARTTIME_GMT'].apply(utc_to_local)
 
-da_df = da_df.pivot(index = 'INTERVALSTARTTIME_PST', columns='LMP_TYPE', values='MW')
+da_df = da_df.pivot(index = 'INTERVALSTARTTIME_GMT', columns='LMP_TYPE', values='MW')
 
 # Add prefix to cols for merging later
 da_df.columns = ["DA_" + col for col in da_df.columns]
@@ -79,15 +78,14 @@ da_df.columns = ["DA_" + col for col in da_df.columns]
 load_df = create_mult_csv_df("./Raw_Data/CAISO_LOAD/")
 
 load_df['INTERVALSTARTTIME_GMT'] = pd.to_datetime(load_df['INTERVALSTARTTIME_GMT'])
-load_df['INTERVALSTARTTIME_PST'] = load_df['INTERVALSTARTTIME_GMT'].apply(utc_to_local)
 
 # FIltering for only total CAISO load for now
 load_df = load_df[load_df['TAC_ZONE_NAME'] == 'Caiso_Totals']
 
 # Getting wide data with import/gen/export as columns
-load_df = load_df.pivot(index='INTERVALSTARTTIME_PST', columns='SCHEDULE', values='MW')
+load_df = load_df.pivot(index='INTERVALSTARTTIME_GMT', columns='SCHEDULE', values='MW')
 
-# Create hourly summary to merge with DA dataset
+# Create hourly summary to merge wwith DA dataset
 hourly_load_df = load_df.groupby(load_df.index.floor("H")).mean()
 
 ## Wind and Solar Forecast ##
@@ -95,24 +93,22 @@ hourly_load_df = load_df.groupby(load_df.index.floor("H")).mean()
 renew_forecast_df = create_mult_csv_df("./Raw_Data/Wind_Solar_Forecast/")
 
 renew_forecast_df['INTERVALSTARTTIME_GMT'] = pd.to_datetime(renew_forecast_df['INTERVALSTARTTIME_GMT'])
-renew_forecast_df['INTERVALSTARTTIME_PST'] = renew_forecast_df['INTERVALSTARTTIME_GMT'].apply(utc_to_local)
 
-renew_forecast_df = renew_forecast_df[renew_forecast_df['LABEL'].isin(['Renewable Forecast Day Ahead', 'Renewable Forecast Actual Generation'])]
+renew_forecast_df = renew_forecast_df[renew_forecast_df['LABEL'] == 'Renewable Forecast RTD']
 
 # Pivoting on hub, renewable type and market
 
-renew_forecast_df = renew_forecast_df.pivot(index='INTERVALSTARTTIME_PST', columns=["TRADING_HUB", "RENEWABLE_TYPE", "LABEL"], values="MW")
+renew_forecast_df = renew_forecast_df.pivot(index='INTERVALSTARTTIME_GMT', columns=["TRADING_HUB", "RENEWABLE_TYPE", "LABEL"], values="MW")
 
 renew_forecast_df.columns = ["_".join(col) for col in renew_forecast_df.columns]
 
 ## Wind and Solar Dispatch ##
-# renew_df = create_mult_csv_df('./Raw_Data/Wind_Solar_Dispatch/')
-# renew_df = renew_df[renew_df['MARKET_RUN_ID'] == 'RTD'] # Filter for only RTD market
-# renew_df.rename({"VALUE":"renewables_rtd"}, axis=1, inplace=True)
-# renew_df['INTERVALSTARTTIME_GMT'] = pd.to_datetime(renew_df['INTERVALSTARTTIME_GMT'])
-# renew_df['INTERVALSTARTTIME_PST'] = renew_df['INTERVALSTARTTIME_GMT'].apply(utc_to_local)
-# renew_df.set_index('INTERVALSTARTTIME_PST', inplace=True)
-# renew_df = renew_df['renewables_rtd']
+renew_df = create_mult_csv_df('./Raw_Data/Wind_Solar_Dispatch/')
+renew_df = renew_df[renew_df['MARKET_RUN_ID'] == 'RTD'] # Filter for only RTD market
+renew_df.rename({"VALUE":"renewables_rtd"}, axis=1, inplace=True)
+renew_df['INTERVALSTARTTIME_GMT'] = pd.to_datetime(renew_df['INTERVALSTARTTIME_GMT'])
+renew_df.set_index('INTERVALSTARTTIME_GMT', inplace=True)
+renew_df = renew_df['renewables_rtd']
 
 ## Importing Weather Data ##
 
@@ -121,7 +117,7 @@ base_url = "https://www.ncei.noaa.gov/access/services/data/v1/?dataset=normals-h
 data_types = "&datatypes=HLY-CLDH-NORMAL, HLY-HTDH-NORMAL, HLY-TEMP-NORMAL"
 # Stations include LAX airport, San Diego Int'l Airport, and Long Beach Airport
 stations="&stations=USW00023174,USW00023188,USW00023129"
-dates="&startDate=2022-08-01T07:00:00Z&endDate=2022-09-04T07:00:00Z"
+dates="&startDate=2022-08-01&endDate=2022-09-04"
 bounding_box="&boundingBox=90,-180,-90,180"
 units="&units=standard"
 
@@ -132,7 +128,6 @@ weather_df = pd.read_csv(StringIO(r.text))
 # Adjusting date formating to match other dataframes
 weather_df['DATE'] = weather_df['DATE'].apply(lambda x: datetime.strptime(x,"%m-%dT%X"))
 weather_df['DATE'] = weather_df['DATE'].apply(lambda x: x.replace(year=2022))
-weather_df['DATE'] = weather_df['DATE'].apply(utc_to_local)
 # weather_df.set_index('DATE', inplace=True)
 
 
@@ -142,11 +137,12 @@ weather_df.drop(axis=1, columns=weather_df.columns[~weather_df.columns.isin(keep
 weather_df = weather_df.pivot(index='DATE', columns='STATION', values=['HLY-CLDH-NORMAL', 'HLY-HTDH-NORMAL', 'HLY-TEMP-NORMAL'])
 
 weather_df.columns = ["_".join(col) for col in weather_df.columns]
+weather_df.index = pd.to_datetime(weather_df.index).tz_localize('GMT')
 
 # Join real-time and day-ahead data
 
 
-df_list = [hourly_rt_df, da_df, hourly_load_df, renew_forecast_df, weather_df]
+df_list = [hourly_rt_df, da_df, hourly_load_df, renew_forecast_df, renew_df, weather_df]
 df = reduce(lambda left, right: pd.merge(left, right, 
                                            left_index=True, 
                                            right_index=True), df_list)
@@ -154,8 +150,6 @@ df = reduce(lambda left, right: pd.merge(left, right,
 
 #### Feature Engineering ####
 # DART Spread, Lagged variables, time of day,
-
-# Create a variable that determines the date at which the DA price applies
 
 df['DART'] = df['DA_LMP'] - df['RT_LMP']
 
@@ -168,18 +162,11 @@ df['weekend']  = np.where(df.index.weekday>4, 1, 0)
 df['hour'] = df.index.hour
 
 # Forecast Error
-df['Total_Solar_Actual'] = df['NP15_Solar_Renewable Forecast Actual Generation'] + df['SP15_Solar_Renewable Forecast Actual Generation'] + df['ZP26_Solar_Renewable Forecast Actual Generation']
-df['Total_Solar_Forecast'] = df['NP15_Solar_Renewable Forecast Day Ahead'] + df['SP15_Solar_Renewable Forecast Day Ahead'] + df['ZP26_Solar_Renewable Forecast Day Ahead']
+df['Total_Solar_Forecast_RTD'] = df['NP15_Solar_Renewable Forecast RTD'] + df['SP15_Solar_Renewable Forecast RTD'] + df['ZP26_Solar_Renewable Forecast RTD']
+df['Total_Wind_Forecast_RTD'] = df['NP15_Wind_Renewable Forecast RTD'] + df['SP15_Wind_Renewable Forecast RTD']
+df['Total_Wind_Solar_Forecast_RTD'] = df['Total_Solar_Forecast_RTD'] + df['Total_Wind_Forecast_RTD']
 
-df['Total_Wind_Actual'] = df['NP15_Wind_Renewable Forecast Actual Generation'] + df['SP15_Wind_Renewable Forecast Actual Generation']
-df['Total_Wind_Forecast'] = df['NP15_Wind_Renewable Forecast Day Ahead'] + df['SP15_Wind_Renewable Forecast Day Ahead']
-
-
-df['Total_Wind_Solar_Actual'] = df['Total_Solar_Actual'] + df['Total_Wind_Actual']
-df['Total_Wind_Solar_Forecast'] = df['Total_Solar_Forecast'] + df['Total_Wind_Forecast']
-
-df['renew_forecast_error'] = df['Total_Wind_Solar_Actual'] - df['Total_Wind_Solar_Forecast']
-
+df['renew_forecast_error'] = df['renewables_rtd'] - df['Total_Wind_Solar_Forecast_RTD']
 
 # Write function to create lagged variables
 
@@ -200,13 +187,14 @@ def create_day_lagged_var(df, lag_var, lag_days=1):
 
 
 # Use function to create lagged variables for forecast error, import, export and generation
+prev_day_forecast_error = create_day_lagged_var(df=df, lag_var='renew_forecast_error', lag_days=1)
+prev_day_load = create_day_lagged_var(df=df, lag_var='renew_forecast_error', lag_days=1)
 
 lag_var_list = ['renew_forecast_error', 'Export', 'Generation', 'Import']
 
 for var in lag_var_list:
     lag_df = create_day_lagged_var(df=df, lag_var=var, lag_days=1)
     df = pd.merge(df, lag_df, left_index=True, right_index=True, how='left')
-
 
 
 # df.to_csv('./Cleaned_Data/dart_and_feature_data.csv')
